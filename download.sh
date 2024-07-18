b31f8282d3f038b5aaf5684b1aa2a69f5c352334
sanskriti-zangoh <sanskriti@zangoh.com>
2024-07-18 17:57:58 +0530

    [+] download example files

---
+++ download.sh
@@ -0,0 +1,9 @@
+mkdir -p example-docs
+
+curl -o example-docs/example-10k.html https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/example-10k.html
+curl -o example-docs/layout-parser-paper-fast.pdf https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/layout-parser-paper-fast.pdf
+curl -o example-docs/DA-1p.jpg https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/DA-1p.jpg
+curl -o example-docs/category-level.docx https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/category-level.docx
+curl -o example-docs/contains-pictures.docx https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/contains-pictures.docx
+curl -o example-docs/docx-tables.docx https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/docx-tables.docx
+curl -o example-docs/embedded-images-tables.pdf https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/embedded-images-tables.pdf
