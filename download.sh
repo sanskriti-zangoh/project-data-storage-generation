12e6f7f29d00fe132577a475a630d6dda81ab53d
sanskriti-zangoh <sanskriti@zangoh.com>
2024-07-19 16:32:16 +0530

    [.] more file samples

--- download.sh
+++ download.sh
@@ -1,9 +1,18 @@
 mkdir -p example-docs
 
 curl -o example-docs/example-10k.html https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/example-10k.html
-curl -o example-docs/layout-parser-paper-fast.pdf https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/layout-parser-paper-fast.pdf
+curl -o example-docs/layout-parser-paper-fast.pdf https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/pdf/layout-parser-paper-fast.pdf
 curl -o example-docs/DA-1p.jpg https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/DA-1p.jpg
 curl -o example-docs/category-level.docx https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/category-level.docx
 curl -o example-docs/contains-pictures.docx https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/contains-pictures.docx
 curl -o example-docs/docx-tables.docx https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/docx-tables.docx
-curl -o example-docs/embedded-images-tables.pdf https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/embedded-images-tables.pdf
+curl -o example-docs/embedded-images-tables.pdf https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/pdf/embedded-images-tables.pdf
+curl -o example-docs/book-war-and-peace-1p.txt https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/book-war-and-peace-1p.txt
+curl -o example-docs/csv-with-escaped-commas.csv https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/csv-with-escaped-commas.csv
+curl -o example-docs/csv-with-long-lines.csv https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/csv-with-long-lines.csv
+curl -o example-docs/emoji.xlsx https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/emoji.xlsx
+curl -o example-docs/factbook.xml https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/factbook.xml
+curl -o example-docs/fake-power-point-many-pages.pptx https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/fake-power-point-many-pages.pptx
+curl -o example-docs/logger.py https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/logger.py
+curl -o example-docs/simple.json https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/simple.json
+curl -o example-docs/simple.yaml https://raw.githubusercontent.com/Unstructured-IO/unstructured/main/example-docs/simple.yaml
