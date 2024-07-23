654726aa077b37dbbc2afeccd0c135504ecfbd98
sanskriti-zangoh <sanskriti@zangoh.com>
2024-07-23 12:35:02 +0530

    [+] questions in list

--- depends/model.py
+++ depends/model.py
@@ -3,6 +3,6 @@ from typing import Optional, Any
 
 def generate_response(prompt: str, system: Optional[str] = None, context: Optional[Any] = None) -> str:
     context = str(context) if context else None
-    new_prompt = f"context: {context}\n\n{prompt}" if context else prompt
+    new_prompt = f"context: {context}\n\ntask: {prompt}" if context else prompt
 
     return generate(model="llama3", prompt=new_prompt, system=system)
--- depends/question_gen.py
+++ depends/question_gen.py
@@ -11,3 +11,15 @@ def generate_questions(elements: List[Element]) :
     pattern = r'\d+\.\s(.+?)\n'
     matches = re.findall(pattern, response['response'])
     return [question for question in matches]
+
+def generate_questions_2(elements: List[Element]) :
+    context = [f"\n\n{element.to_dict()['type']}:\n{element.to_dict()['text']}\page_number: {element.to_dict()['metadata']['page_number']}\nposition_in_page: {element.to_dict()['metadata']['coordinates']}" for element in elements]
+    metadata = {"languages": elements[0].to_dict()['metadata']['languages'], "filetype": elements[0].to_dict()['metadata']['filetype']}
+    context = f"metadata:\n{metadata}\n\njson_ended_document:\n{context}"
+    prompt = "You are an expert and you are formulating questions from the document to assess the knowledge of a student about content given in the json encoded document. Please formulate as many questions as possible to assess knowledge of the document given."
+    system = "You always give response in pure numbered list"
+    response = generate_response(prompt=prompt, system=system, context=context)
+    pattern = r'\d+\.\s(.+?)\n'
+    matches = re.findall(pattern, response['response'])
+    return [question for question in matches]
+
